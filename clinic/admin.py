2186ed8bd29e0bed24056e80ee4d69f886e6ec2c
thonyatiaung <thonyatiaung999@gmail.com>
2021-11-26 14:49:12 +0630

    first commit

---
+++ clinic/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Clinic
+class ClinicAdmin(admin.ModelAdmin):
+    list_display = ['patient_name', 'patient_phone', 'patient_address', 'patient_case', 'drug', 'fees', 'doctor', 'datetime']
+    list_filter = ['patient_address', 'patient_case', 'doctor', 'drug', 'fees', 'datetime']
+    search_fields = ['patient_name']
+
+admin.site.register(Clinic, ClinicAdmin)   
